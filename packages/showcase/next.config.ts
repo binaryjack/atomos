5327dc756f0e6ecd0a8bc4edfa02b2c6919df5bb
BinaryJack <pianatadeo@hotmail.com>
2026-05-07 09:24:40 +0200

    fixed config

--- packages/showcase/next.config.ts
+++ packages/showcase/next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  distDir: "out", // this is the default
   basePath: '/atomos-monorepo',
   assetPrefix: '',
+  images: {
+    // Allows next/image to be used without 3rd party optimization service. - necessary for static export
+    unoptimized: true,
+  },
   experimental: {
   },
 };
