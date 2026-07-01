e0f750038c883ff877bad8d69327d55e06b4a9c2
binaryjack <pianatadeo@hotmail.com>
2026-07-01 11:31:33 +0200

    chore: deploy latest showcase and bump versions to 2.3.0

---
+++ docs/_next/static/INCWSN16M8bz9P4R49Jd5/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/atomos//_next/:path+",
+        "destination": "/atomos/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
